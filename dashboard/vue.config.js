fd04b67bb0c8a3668effe3a06a01d95e1010bf07
Olivier <hi@olivierodo.me>
2021-05-13 14:01:29 +0700

    update dockerfile #77

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -1,5 +1,7 @@
+const path = require('path')
 module.exports = {
   publicPath: '/',
+  outputDir: path.resolve(__dirname, '../dist'),
   transpileDependencies: [
     'vue-echarts',
     'resize-detector'
