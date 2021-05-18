99736f9db76826300fe7d80f38438ee47ad1856d
Olivier <hi@olivierodo.me>
2021-05-18 19:54:07 +0700

    refactor dashboard test

---
+++ jest.setup.js
@@ -0,0 +1,87 @@
+const path = require('path')
+const os = require('os')
+const fs = require('fs')
+const rimraf = require('rimraf')
+
+global.JestQA = function(filename, unmount = false, debug = false) {
+  const DEFAULT_CONFIG = path.resolve(process.cwd(), '.restqa.yml') 
+  const testName = path.basename(filename).replace('.test.js', '')
+  const tests = []
+
+  let current
+
+  const _log = function(msg) {
+    (debug ||  current.debug) && console.log(`/JESTSQA/{ ${testName} } ->`, msg)
+    return true
+  }
+
+  const create = function(content, filename) {
+    _log(`creating file ${filename}`)
+    const folder = path.dirname(filename)
+    fs.mkdirSync(folder, {recursive: true})
+    fs.writeFileSync(filename, content)
+    current.files.push(filename)
+    _log(`File ${filename} created`)
+    return filename
+  }
+
+  const createTmpFile = function(content, file) {
+    filename = path.resolve(os.tmpdir(), testName, current.id, file)
+    return create(content, filename)
+  }
+
+  const createCwdConfig = function(content) {
+    return create(content, DEFAULT_CONFIG)
+  }
+
+  const clean = function() {
+    if (true === unmount) {
+      _log(`unmount mock`)
+      jest.resetModules()
+      jest.resetAllMocks()
+    }
+
+    current.files
+      .forEach(file => {
+        _log(`deleting file ${file}`)
+        fs.existsSync(file) && fs.unlinkSync(file)
+      })
+  }
+
+  const hooks = Object.seal({
+    beforEach: null,
+    afterEach: null
+  })
+
+  const beforeEach = function() {
+    current = Object.seal({
+      id: testName + '-' + Math.floor(Math.random() * 10000000) + '-' + Date.now(),
+      files: [
+        DEFAULT_CONFIG
+      ],
+      folders: [],
+      debug: false
+    })
+    tests.push(current)
+    _log(`Creating test ${current.id}`)
+    clean()
+
+    hooks.beforeEach && _log('calling hook beforeEach') && hooks.beforeEach.call(this)
+  }
+
+  const afterEach = function() {
+    clean()
+    hooks.afterEach && _log('calling hook afterEach') && hooks.afterEach.call(this)
+    current = null
+  }
+
+  return {
+    beforeEach,
+    afterEach,
+    getCurrent: () => current,
+    createTmpFile,
+    createCwdConfig,
+    hooks,
+  }
+
+}
