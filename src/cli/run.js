20ba06a981d81dafcb146489ac99fce02b72d9f8
Olivier <hi@olivierodo.me>
2021-07-18 12:24:32 +0300

    bump: Upgrade cucumber@6 -> @cucumber/cucumber@7

--- src/cli/run.js
+++ src/cli/run.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const fs = require('fs')
-const cucumber = require('cucumber')
+const cucumber = require('@cucumber/cucumber')
 const logger = require('../utils/logger')
 
 module.exports = function (opt, program = {}) {
