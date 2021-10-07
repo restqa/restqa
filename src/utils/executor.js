ef7af73ebee036b58ce06bb5dfeafc7c9e04e21f
tony-go <gorez.tony@gmail.com>
2021-10-07 18:25:01 +0200

    fix: switch to spawn an write tests

--- src/utils/executor.js
+++ src/utils/executor.js
@@ -1,25 +1,38 @@
-const execa = require("execa");
+const spawn = require("cross-spawn");
 const logger = require("../utils/logger");
 
 module.exports = {
   /**
    * 
    * @param {string} command
+   * @param {AbortController} controller
 
    */
-  execute: async function executeCommand(command) {
-    if (typeof command === "string") {
-      try {
-        const child = await execa.command(command);
-        process.stdout.write(child.stdout);
-        logger.success(`Server is running ${command}`)
-      } catch (error) {
-        process.stdout.write(error.message);
-        throw new Error(`Command exited, code: ${error.code || error.exitCode}`);
+  execute:  async function executeCommand(command, controller) {
+    return new Promise((resolve, reject) => {
+      if (typeof command === "string") {
+        const server = spawn(command, {
+          shell: true,
+          signal: controller ? controller.signal : undefined
+        });
+        
+        server.stdout.pipe(process.stdout)
+        server.stdout.on("data", () => {
+          logger.success(`Server is running ${command}`)
+          resolve();
+        })
+
+        server.stderr.on("data", (data) => {
+          reject(new Error(`Error during running command ${command}`))
+        });
+
+        server.on("close", () => {
+          logger.info("Server closed!")
+        })
       }
-    }
-    else {
-      throw new Error(`Executor: command should be a string but received ${typeof command}`);
-    }
+      else {
+        throw new Error(`Executor: command should be a string but received ${typeof command}`);
+      }
+    });
   }
 }
--- src/utils/executor.test.js
+++ src/utils/executor.test.js
@@ -1,7 +1,12 @@
+const { setTimeout } = require("timers/promises")
+
 const {execute} = require("./executor");
 
 describe("# utils - executor", () => {
-  const stdoutSpy = jest.spyOn(process.stdout, "write")
+  // Mocks and spies
+  const stdoutSpy = jest.spyOn(process.stdout, "write").mockImplementation(jest.fn());
+  const loggerInfoSpy = jest.spyOn(require("../utils/logger"), "info").mockImplementation(jest.fn());;
+  const loggerSuccessSpy = jest.spyOn(require("../utils/logger"), "success").mockImplementation(jest.fn());;
 
   test("given an valid command when we execute it then process.stdout.write should have been called", async() => {
     // Given
@@ -11,7 +16,19 @@ describe("# utils - executor", () => {
     await execute(validCommand);
 
     // Then
+    expect(loggerSuccessSpy).toHaveBeenCalledWith(`Server is running ${validCommand}`)
     expect(stdoutSpy).toHaveBeenCalled();
+  });
+
+  test("given a valid command when execute it then it should display a success message", async () => {
+    // Given
+    const validCommand = "ls -l";
+
+    // When
+    await execute(validCommand);
+
+    // Then
+    expect(loggerSuccessSpy).toHaveBeenCalledWith(`Server is running ${validCommand}`);
   });
 
   test("given a mistyped command when we execute it then it should throw an error", async() => {
@@ -29,34 +46,32 @@ describe("# utils - executor", () => {
     }
   });
 
-  test("given a valid command when execute it then it should display a success message", async () => {
-    // Mocks and spies
-    const loggerSpy = jest.spyOn(require("../utils/logger"), "success");
-
-    // Given
-    const validCommand = "ls -l";
-
-    // When
-    await execute(validCommand);
-
-    // Then
-    expect(loggerSpy).toHaveBeenCalledWith(`Server is running ${validCommand}`);
-  });
-
   test("given a command that should fail when we execute it then it should throw an error", async() => {
     // Given
-    const commandShouldFail = "ls dd-sdd";
+    const commandShouldFail = "cd dsqsq";
 
     // When
-    expect.assertions(2);
+    expect.assertions(1);
     try {
-      await execute(commandShouldFail)
+      await execute(commandShouldFail);
     } catch(error) {
       // Then
       // eslint-disable-next-line
-      expect(error).toEqual(new Error(`Command exited, code: 1`));
-      // eslint-disable-next-line
-      expect(stdoutSpy).toHaveBeenCalled();
+      expect(error).toEqual(new Error(`Error during running command ${commandShouldFail}`));
     }
+  });
+
+  test("given a watch command, AbortController when we execute it then we could abort it", async() => {
+    // Given
+    const watchCommand = "htop";
+    const controller = new AbortController();
+
+    // When
+    await execute(watchCommand, controller);
+    await setTimeout(3000);
+    controller.abort();
+
+    // Then
+    expect(loggerInfoSpy).toHaveBeenCalledWith("Server closed!");
   });
 });
