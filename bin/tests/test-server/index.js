5ad949f2ea206b91b87697eaf603ddf131969c2d
tony-go <gorez.tony@gmail.com>
2021-10-10 15:34:01 +0200

    fix: add autoclose server for test-server

--- bin/tests/test-server/index.js
+++ bin/tests/test-server/index.js
@@ -4,4 +4,13 @@ const app = fastify();
 
 app.get("/hello", () => ({value: "world"}));
 
-app.listen(9999);
+app.listen(9999, () => {
+  forceClose();
+});
+
+// Hack: force close (due to bats cannot kill a subprocess)
+function forceClose() {
+  setTimeout(() => {
+    app.close();
+  }, 10000);
+}
