64fff1fa0ff588bdfc9da14b64b6ba2d011ecbb0
tony-go <gorez.tony@gmail.com>
2021-10-22 17:42:49 +0200

    chore: clean express test (-100 lines)

--- src/hooks/express.test.js
+++ src/hooks/express.test.js
@@ -6,35 +6,47 @@ const got = require("got")
 const expressHook = require("./express");
 
 describe("Express hooks", () => {
-  test("Send Event on each request that passing (GET)", async () => {
-    const message = "hello world";
-    const opt = {
-      configFile: false,
-      sandbox: new EventEmitter()
-    };
+  let app;
+  let httpClient;
+  let serverPort;
 
-    const app = express().use(express.json());
-    expressHook(app, opt);
+  const port = 0
+  const message = "hello world";
+  const options = {
+    configFile: false,
+    sandbox: new EventEmitter()
+  };
+  
+  beforeAll(async() => {
+    app = express().use(express.json());
+    expressHook(app, options);
     app.get("/hello", (req, res) => {
       return res.json({message});
     });
-
-    const server = app.listen(0);
-    const port = server.address().port;
-
-    const instance = got.extend({
-      prefixUrl: `http://127.0.0.1:${port}`,
-      throwHttpErrors: false
+    app.post("/greeting", (req, res) => {
+      return res.status(201).json({message});
     });
 
-    const requestAPI = instance.get("hello", {responseType: "json"});
-    const requestDashboard = instance.get("restqa");
-    const promiseEvent = once(opt.sandbox, "request");
+    const server = app.listen(port);
+
+    serverPort = server.address().port;
+    httpClient = got.extend({
+      prefixUrl: `http://127.0.0.1:${serverPort}`,
+      throwHttpErrors: false
+    });
+  });
+
+  afterAll(async() => {
+    await app.close()
+  })
+
+  test("Send Event on each request that passing (GET)", async () => {
+    const requestAPI = httpClient.get("hello", {responseType: "json"});
+    const requestDashboard = httpClient.get("restqa");
+    const promiseEvent = once(options.sandbox, "request");
 
     const [responseAPI, responseDashboard, emittedEvent] =
       await Promise.all([requestAPI, requestDashboard, promiseEvent]);
-
-    server.close();
 
     expect(responseAPI.statusCode).toBe(200);
     expect(responseAPI.body.message).toBe(message);
@@ -52,7 +64,7 @@ describe("Express hooks", () => {
           accept: "application/json",
           "accept-encoding": "gzip, deflate, br",
           connection: "close",
-          host: "127.0.0.1:" + port,
+          host: "127.0.0.1:" + serverPort,
           "user-agent": "got (https://github.com/sindresorhus/got)"
         },
         body: {}
@@ -74,36 +86,14 @@ describe("Express hooks", () => {
   });
 
   test("Send Event on each request that passing (GET + query parameters)", async () => {
-    const message = "hello world";
-    const opt = {
-      configFile: false,
-      sandbox: new EventEmitter()
-    };
-
-    const app = express().use(express.json());
-    expressHook(app, opt);
-    app.get("/hello", (req, res) => {
-      return res.json({message});
-    });
-
-    const server = app.listen(0);
-    const port = server.address().port;
-
-    const instance = got.extend({
-      prefixUrl: `http://127.0.0.1:${port}`,
-      throwHttpErrors: false
-    });
-
-    const requestAPI = instance.get("hello?foo=bar&first=parameter", {
+    const requestAPI = httpClient.get("hello?foo=bar&first=parameter", {
       responseType: "json"
     });
-    const requestDashboard = instance.get("restqa");
-    const promiseEvent = once(opt.sandbox, "request");
+    const requestDashboard = httpClient.get("restqa");
+    const promiseEvent = once(options.sandbox, "request");
 
     const [responseAPI, responseDashboard, emittedEvent] =
       await Promise.all([requestAPI, requestDashboard, promiseEvent]);
-
-    server.close();
 
     expect(responseAPI.statusCode).toBe(200);
     expect(responseAPI.body.message).toBe(message);
@@ -126,7 +116,7 @@ describe("Express hooks", () => {
           accept: "application/json",
           "accept-encoding": "gzip, deflate, br",
           connection: "close",
-          host: "127.0.0.1:" + port,
+          host: "127.0.0.1:" + serverPort,
           "user-agent": "got (https://github.com/sindresorhus/got)"
         }
       },
@@ -147,39 +137,18 @@ describe("Express hooks", () => {
   });
 
   test("Send Event on each request that passing (get + header parameters)", async () => {
-    const message = "hello world";
-    const opt = {
-      configFile: false,
-      sandbox: new EventEmitter()
-    };
-    const app = express().use(express.json());
-   expressHook(app, opt);
-    app.get("/hello", (req, res) => {
-      return res.json({message});
-    });
-
-    const server = app.listen(0);
-    const port = server.address().port;
-
-    const instance = got.extend({
-      prefixUrl: `http://127.0.0.1:${port}`,
-      throwHttpErrors: false
-    });
-
-    const requestAPI = instance.get("hello", {
+    const requestAPI = httpClient.get("hello", {
       responseType: "json",
       headers: {
         "x-api-key": "xxx-yyy-zzz",
-        "accept-type": "applicaiton/json"
+        "accept-type": "application/json"
       }
     });
-    const requestDashboard = instance.get("restqa");
-    const promiseEvent = once(opt.sandbox, "request");
+    const requestDashboard = httpClient.get("restqa");
+    const promiseEvent = once(options.sandbox, "request");
 
     const [responseAPI, responseDashboard, emittedEvent] =
       await Promise.all([requestAPI, requestDashboard, promiseEvent]);
-
-    server.close();
 
     expect(responseAPI.statusCode).toBe(200);
     expect(responseAPI.body.message).toBe(message);
@@ -197,10 +166,10 @@ describe("Express hooks", () => {
           "x-api-key": "xxx-yyy-zzz",
           accept: "application/json",
           "accept-encoding": "gzip, deflate, br",
-          "accept-type": "applicaiton/json",
+          "accept-type": "application/json",
           "user-agent": "got (https://github.com/sindresorhus/got)",
           connection: "close",
-          host: "127.0.0.1:" + port
+          host: "127.0.0.1:" + serverPort
         },
         query: {},
         body: {}
@@ -222,82 +191,28 @@ describe("Express hooks", () => {
   });
 
   test("Send Event on each request that passing (post + json body)", async () => {
-    const message = "hello world";
     const opt = {
-      configFile: false,
-      sandbox: new EventEmitter(),
+      ...options,
       route: "/jestqa"
     };
+    const localApp = express().use(express.json());
+    expressHook(localApp, opt)
+    const localServer = localApp.listen(0);
+    const port = localServer.address().port;
 
-    const app = express().use(express.json());
-    expressHook(app, opt).post("/greeting", (req, res) => {
-      return res.status(201).json({message});
-    });
-
-    const server = app.listen(0);
-    const port = server.address().port;
-
-    const instance = got.extend({
+    const localHttpClient = got.extend({
       prefixUrl: `http://127.0.0.1:${port}`,
       throwHttpErrors: false
     });
 
-    const requestAPI = instance.post("greeting", {
-      json: {
-        firstName: "John",
-        lastName: "Doe"
-      },
-      responseType: "json"
-    });
 
-    const requestDashboard = instance.get("jestqa");
-    const promiseEvent = once(opt.sandbox, "request");
+    const responseDashboard = await localHttpClient.get("jestqa");
 
-    const [responseAPI, responseDashboard, emittedEvent] =
-      await Promise.all([requestAPI, requestDashboard, promiseEvent]);
-
-    server.close();
-
-    expect(responseAPI.statusCode).toBe(201);
-    expect(responseAPI.body.message).toBe(message);
+    localServer.close();
 
     expect(responseDashboard.statusCode).toBe(200);
     expect(responseDashboard.headers["content-type"]).toBe(
       "text/html; charset=UTF-8"
     );
-
-    const expectedMessage = expect.objectContaining({
-      request: {
-        method: "POST",
-        path: "/greeting",
-        headers: {
-          "content-type": "application/json",
-          accept: "application/json",
-          "accept-encoding": "gzip, deflate, br",
-          connection: "close",
-          "content-length": "37",
-          host: "127.0.0.1:" + port,
-          "user-agent": "got (https://github.com/sindresorhus/got)"
-        },
-        query: {},
-        body: {
-          firstName: "John",
-          lastName: "Doe"
-        }
-      },
-      response: {
-        statusCode: 201,
-        headers: {
-          "content-length": "25",
-          "content-type": "application/json; charset=utf-8",
-          "x-powered-by": "Express"
-        },
-        body: {
-          message
-        }
-      }
-    });
-    expect(emittedEvent).toHaveLength(1);
-    expect(emittedEvent[0]).toEqual(expectedMessage);
   });
 });
