8583774e8560bdb46e49d755420194ed30c4c5c1
tony-go <gorez.tony@gmail.com>
2021-10-22 16:40:36 +0200

    chore: move back documentation

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,34 +1,5 @@
 const expressHook = require("./express");
 
-/**
- * Expose middleware for Express and Fastify
- *
- * @param {Object} options
- * @param {string} options.configFile - The path of the RestQA configuration file
- * @param {string} (optional) options.folder - Define the folder where to project is located
- * @param {string} (optional) options.readOnly - Restrict the access to the feature file into read only
- * @param {string} (optional) options.route - Route used to exposed the dashboard (default: '/restqa')
- *
- * @return http.server
- *
- * @example
- *
- * const express = require('express')
- * const { Hooks } = require('@restqa/restqa')
- *
- * const options = {
- *   configFile: './restqa.yml',
- *   folder: '/app/project',
- *   readOnly: true,
- *   route: '/restqa'
- * }
- *
- * const server = express()
- * Hooks.express(server, options)
- * server.listen(8000, () => {
- *   console.log('The Microservice is running on the port 8000')
- * })
- */
 module.exports = {
   express: expressHook
 }
