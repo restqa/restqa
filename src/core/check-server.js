a084cdf996fadad8d4599ba4003bfacfd3d2ce09
Olivier <hi@olivierodo.me>
2021-10-31 08:58:21 +0700

    chore: Refactor the executor in order to run it at the hook level

--- src/core/check-server.js
+++
@@ -1,7 +0,0 @@
-const promisifySetTimeout = require("util").promisify(setTimeout);
-
-async function checkServer() {
-  await promisifySetTimeout(3000);
-}
-
-module.exports = {checkServer};
--- src/core/executor.js
+++ src/core/executor.js
@@ -1,21 +1,28 @@
 const spawn = require("cross-spawn");
 const logger = require("../utils/logger");
+const net = require("net");
+const Locale = require("../locales")("service.run");
 
 module.exports = {
   /**
    *
    * @param {string} command
+   * @param {object} envs
    */
-  execute: async function executeCommand(command) {
+  execute: async function executeCommand(command, envs) {
     return new Promise((resolve, reject) => {
       if (typeof command === "string") {
         let initialized = false;
         const server = spawn(command, {
-          shell: true
+          shell: true,
+          envs: {
+            ...process.env,
+            ...envs
+          }
         });
 
         // reject if an error happened
-        server.stderr.on("data", () => {
+        server.stderr.on("data", (er) => {
           if (!initialized) {
             initialized = true;
             reject(new Error(`Error during running command ${command}`));
@@ -47,6 +54,31 @@ module.exports = {
           `Executor: command should be a string but received ${typeof command}`
         );
       }
+    });
+  },
+  checkServer: async function (port, timeout = 4000) {
+    return new Promise((resolve, reject) => {
+      const checker = () => {
+        const socket = net.createConnection({port});
+        socket.on("ready", function (err) {
+          if (err) reject(err);
+          resolve();
+          socket.destroy();
+        });
+        socket.on("error", function (err) {
+          socket.destroy();
+          timeout -= 200;
+          if (err.code !== "ECONNREFUSED") {
+            return reject(err);
+          }
+          if (timeout > 0) {
+            setTimeout(checker, 200);
+          } else {
+            reject(new Error(Locale.get("error_port_timeout")));
+          }
+        });
+      };
+      checker();
     });
   }
 };
