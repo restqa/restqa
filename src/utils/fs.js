38c9e090d32147f1008bae59bea70085410252fe
tony-go <gorez.tony@gmail.com>
2021-11-09 18:05:39 +0100

    fix: add default parameter to getPackageJson utils

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const fs = require("fs");
 
-function getPackageJson() {
-  const packageJsonPath = path.resolve(process.cwd(), "package.json");
+function getPackageJson(pathSegment = process.cwd()) {
+  const packageJsonPath = path.resolve(pathSegment, "package.json");
   if (typeof packageJsonPath === "string" && fs.existsSync(packageJsonPath)) {
     const rawPkg = fs.readFileSync(packageJsonPath).toString("utf-8");
     return JSON.parse(rawPkg);
