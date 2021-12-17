d09590df6908bb5bdc6055f4cbaada264d9f0f63
Olivier <hi@olivierodo.me>
2021-12-17 21:54:06 +0700

    chore: Delete the restqa example command

--- src/cli/example.js
+++
@@ -1,9 +0,0 @@
-const Run = require("./run");
-const path = require("path");
-
-module.exports = async function () {
-  const options = {
-    args: [path.resolve(__dirname, "..", "..", "example")]
-  };
-  return Run(options);
-};
