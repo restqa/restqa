daa855051c1f6783f8c1912b7e9785a8836ccf5a
Olivier <hi@olivierodo.me>
2022-05-14 08:50:31 +0700

    feat: generate the open api specification from the test scenarios

---
+++ src/core/specification/index.test.js
@@ -0,0 +1,13 @@
+describe("#lib - specification", () => {
+  test("throw an error if the configuration doesn't contains the tool", () => {
+    const Specification = require("./index");
+    const config = {
+      specification: {}
+    }
+    expect(() => new Specification(config)).toThrow(
+      'The specification property "tool" should be specify. (available: swagger)'
+    );
+  });
+});
+
+
