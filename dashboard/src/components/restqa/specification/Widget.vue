681824c408d861d848d7596b7383b3db9a103630
Olivier <hi@olivierodo.me>
2022-05-14 09:36:26 +0700

    Feature: enable the api specification into the dashboard

---
+++ dashboard/src/components/restqa/specification/Widget.vue
@@ -0,0 +1,40 @@
+<template>
+  <card title="API specification" emoji="📚" class="card" v-if="specification.enabled">
+    <img :src="require(`@/assets/images/logos/${config.specification.tool}.png`)" class="option-img" />
+    <br /><br />
+    <el-link type="primary" v-on:click.stop.prevent="goTo()">Access to your API specification</el-link>
+  </card>
+</template>
+
+<script>
+import Card from "@/components/UI/card/Card";
+
+export default {
+  name: "SpecificationWidget",
+  components: {
+    Card
+  },
+  data() {
+    const specification = this.$store.getters.projectStatus.specification;
+    const config = this.$store.getters.projectConfiguration
+    return {
+      specification,
+      config
+    };
+  },
+  methods: {
+    goTo() {
+      this.$router.push({name: 'specification' })
+    }
+  }
+}
+</script>
+<style scoped>
+  .card {
+    text-align: center;
+  }
+  img {
+    width: 100%;
+  }
+</style>
+
