2e960f0cf4c7985dde3894def95cb96cf90ffe79
Olivier <hi@olivierodo.me>
2022-05-14 09:36:26 +0700

    Feature: enable the api specification into the dashboard

--- dashboard/src/views/openapi/Openapi.vue
+++ dashboard/src/views/openapi/Openapi.vue
@@ -3,20 +3,32 @@
     <el-breadcrumb-item :to="{name: 'homepage'}">Dashboard</el-breadcrumb-item>
     <el-breadcrumb-item>API Specification</el-breadcrumb-item>
   </el-breadcrumb>
-  <div class="swagger" id="swagger"></div>
+  <h2><i class="el-icon-collection" /> API Specification</h2>
+  <el-row :gutter="20">
+    <el-col :span="24" v-if="specification.enabled === false">
+      <editor></editor>
+    </el-col>
+    <el-col :span="24" v-else>
+      <specification></specification>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-import SwaggerUI from "swagger-ui";
-import "swagger-ui/dist/swagger-ui.css";
+import Editor from "@/components/restqa/specification/Editor.vue";
+import Specification from "@/components/restqa/specification/Specification.vue";
 
 export default {
+  components: {
+    Editor,
+    Specification
+  },
   name: "Openapi",
-  mounted() {
-    SwaggerUI({
-      spec: window.RESTQA_OPENAPI,
-      dom_id: "#swagger"
-    });
-  }
+  data() {
+    const specification = this.$store.getters.projectStatus.specification;
+    return {
+      specification
+    }
+  },
 };
 </script>
