d93b0ac7f2fbf58e74fecc661a51ff9d0e2106eb
Olivier <hi@olivierodo.me>
2022-11-15 12:12:53 +0700

    cleanup: example typescript folder

--- examples/typescript/src/index.ts
+++ examples/typescript/src/index.ts
@@ -1,18 +1,21 @@
-import express from 'express'
+import express, { Express, Request, Response } from 'express';
 
 const { PORT = 9000 } = process.env
-const server = express();
- .get('/', (req, res) => {
+const server = express()
+ .get('/', (req: Request, res: Response) => {
     res.json({
       hello: 'world'
     })
   })
   .listen(PORT, () => {
     console.log('server run on port ' + PORT)
+    setTimeout(() => {
+      process.kill(process.pid, 'SIGTERM')
+    }, 2000)
   })
 
 process.on('SIGTERM', () => {
   server.close(() => {
-    console.log('server is closing')
+    console.log('---- server is closing')
   })
 })
