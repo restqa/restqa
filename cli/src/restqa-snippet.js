ac45e9aaa9506bc59dcc619cf382e8764a1bdf2c
Olivier <hi@olivierodo.me>
2022-11-17 19:54:57 +0700

    feat: enable recommendation when a step definition is incorrect

--- cli/src/restqa-snippet.js
+++ cli/src/restqa-snippet.js
@@ -1,4 +1,6 @@
 const chalk = require("chalk");
+const fsm = require("fuzzy-string-matching");
+const Steps = require("./cli/steps");
 
 function RestQASnippetSyntax(snippetInterface) {
   this.snippetInterface = snippetInterface;
@@ -10,23 +12,48 @@ RestQASnippetSyntax.prototype.build = function ({
   functionName,
   stepParameterNames
 }) {
-  const result = [
+  const {configFile} = global.restqa;
+  const keyword = functionName.toLowerCase();
+  const restqaSteps = Steps(keyword, {config: configFile, print: false});
+  const steps = generatedExpressions.map((step) => {
+    step.matchs = restqaSteps
+      .filter(({Step}) => {
+        const match = fsm(Step, step.expressionTemplate);
+        return match > 0.2;
+      })
+      .map((_) => _.Step);
+    return step;
+  });
+
+  const result = {};
+  result.headers = ["", "😵 Oooppps! The steps does not exist:"];
+
+  result.body = steps
+    .map((step) => {
+      let result = [
+        '"""',
+        functionName + " " + step.expressionTemplate,
+        '"""',
+        ""
+      ];
+      if (step.matchs.length > 0) {
+        result = result.concat([
+          "Did you mean:",
+          step.matchs.map((x) => "- " + functionName + " " + x).join("\n"),
+          "",
+          "---"
+        ]);
+      }
+      return result;
+    })
+    .flat();
+
+  result.footer = [
     "",
-    `😵 Oooppps! We are facing an issue with the step${
-      generatedExpressions.length > 1 ? "s" : ""
-    }:`,
-    '"""',
-    generatedExpressions.map((_) => _.expressionTemplate).join("\n"),
-    '"""',
-    "",
-    "Do not worry! We are here to find a solution for the error your encountering. Please try to check the following:",
-    "---",
+    "If the problem persist, please try to check the following:",
     "👉 Check if you don't have any random spaces at the begining, in the middle or at the end of the line.",
     `👉 Check the available steps on your project by using the command: ${chalk.bold.cyan(
       "restqa steps " + functionName.toLowerCase()
-    )}`,
-    `👉 Look at the documentation: ${chalk.yellow(
-      "https://docs.restqa.io/tests/introduction"
     )}`,
     `👉 Get more support by contacting us: ${chalk.yellow(
       "https://restqa.io/chat"
@@ -35,13 +62,15 @@ RestQASnippetSyntax.prototype.build = function ({
     "or...",
     "",
     `🚀 Create you own step definition: ${chalk.yellow(
-      "https://docs.restqa.io/plugin/introduction"
+      "https://docs.restqa.io"
     )}`,
     "",
     "Thank you for using RestQA! 💜"
-  ].join("\n");
+  ];
 
-  return chalk.redBright(result);
+  return chalk.redBright(
+    [result.headers, result.body, result.footer].flat().join("\n")
+  );
 };
 
 module.exports = RestQASnippetSyntax;
