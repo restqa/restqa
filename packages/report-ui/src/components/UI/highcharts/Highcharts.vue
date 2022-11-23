63a746545388de802901d34a07753378483d1da1
Olivier <hi@olivierodo.me>
2022-11-23 09:25:22 +0700

    UI: lazy load the content from vendors (swagger / highcharts)

--- packages/report-ui/src/components/UI/highcharts/Highcharts.vue
+++ packages/report-ui/src/components/UI/highcharts/Highcharts.vue
@@ -1,8 +1,17 @@
 <template>
-  <div ref="container"></div>
+  <el-alert
+    title="An error occured while trying to load the Highcharts library..."
+    type="error"
+    :closable="false"
+    class="error"
+    v-if="hasError"
+  />
+  <div ref="container" v-loading="loading"></div>
 </template>
 
 <script>
+import { LoadScript } from "@/services/utils/lazy-loader.js";
+
 export default {
   name: "UiHighcharts",
   props: {
@@ -14,10 +23,26 @@ export default {
   data() {
     return {
       chart: null,
+      loading: true,
+      hasError: false,
     };
   },
+  methods: {
+    initiate() {
+      false;
+      this.chart = Highcharts.chart(this.$refs.container, this.options);
+    },
+  },
   mounted() {
-    this.chart = Highcharts.chart(this.$refs.container, this.options);
+    if (undefined === window.Highcharts) {
+      LoadScript("./vendors/highcharts.js")
+        .then(this.initiate)
+        .catch(() => {
+          this.hasError = true;
+        });
+    } else {
+      this.initiate();
+    }
   },
   watch: {
     options: {
@@ -28,3 +53,8 @@ export default {
   },
 };
 </script>
+<style scoped>
+.error {
+  margin-bottom: 30px;
+}
+</style>
