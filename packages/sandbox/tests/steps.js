8143b1a9722af1199dfe646b5e424749e03ed079
Olivier <hi@olivierodo.me>
2022-12-02 17:44:21 +0700

    [SANDBOX] CI: fix the build on nodejs16 unix... For some reason fatisfy is hanging... And makes all the test fail...

--- packages/sandbox/tests/steps.js
+++ packages/sandbox/tests/steps.js
@@ -2,30 +2,25 @@ const fastify = require("fastify");
 const fs = require("fs");
 const path = require("path");
 const assert = require("assert");
+const http = require("http");
 
 const Steps = function ({Given, When, Then}) {
   Given(
     "the upstream path {string} {string} should respond:",
     function (method, url, response) {
-      const app = fastify({logger: false});
-      app.route({
-        method,
-        url,
-        handler: function (request, reply) {
-          try {
-            const res = JSON.parse(response);
-            reply.send(res);
-          } catch (e) {}
-          reply.send(response);
-        }
-      });
-
-      this.upstream = app;
+      if (process.platform !== "win32" && process.version.includes("v16")) {
+        const err =
+          "The tests are super slow on Node 16.... Save yourself some time! Upgrade to Node18";
+        console.log(err); // eslint-disable-line no-console
+        this.upstream = getServerNode16(method, url, response);
+      } else {
+        this.upstream = getServer(method, url, response);
+      }
     }
   );
 
-  When("upstream starts on port {int}", async function (port) {
-    return this.upstream.listen({port});
+  When("upstream starts on port {int}", function (port, cb) {
+    this.upstream.start(port, cb);
   });
 
   When("upstream stops", function (cb) {
@@ -62,5 +57,60 @@ const Steps = function ({Given, When, Then}) {
     assert.ok(content.includes(expectedContent), expectedContent);
   });
 };
+
+function getServerNode16(method, url, response) {
+  const handler = function (req, res) {
+    res.writeHead(200);
+    try {
+      response = JSON.stringify(JSON.parse(response));
+      res.setHeader("Content-Type", "application/json");
+    } catch (e) {}
+    return res.end(response);
+  };
+
+  const srv = http.createServer(handler);
+
+  function start(port, cb) {
+    srv.listen(port, cb);
+  }
+
+  function close(cb) {
+    srv.close(cb);
+  }
+
+  return {
+    start,
+    close
+  };
+}
+
+function getServer(method, url, response) {
+  const srv = fastify({logger: false});
+
+  srv.route({
+    method,
+    url,
+    handler: function (request, reply) {
+      try {
+        const res = JSON.parse(response);
+        reply.send(res);
+      } catch (e) {}
+      reply.send(response);
+    }
+  });
+
+  function start(port, cb) {
+    srv.listen({port}, cb);
+  }
+
+  function close(cb) {
+    srv.close(cb);
+  }
+
+  return {
+    start,
+    close
+  };
+}
 
 module.exports = Steps;
