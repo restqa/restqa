76b3f25fe8b5e6d53c5f84cda8a247a4909f39db
Tony Gorez <gorez.tony@gmail.com>
2022-12-08 08:40:07 +0100

    fix: increase default boot time to 120000

--- packages/plugin-http-mock/src/index.js
+++ packages/plugin-http-mock/src/index.js
@@ -4,7 +4,7 @@ const RestQAHTTPMock = require("./http-mock");
 const instance = new RestQAPlugin(RestQAHTTPMock.name);
 instance
   .addBeforeAllHook(
-    {timeout: Number(process.env.BOOT_TIMEOUT) || 30000},
+    {timeout: Number(process.env.BOOT_TIMEOUT) || 120000},
     function () {
       const config = instance._config;
       return RestQAHTTPMock.hooks.beforeAll.call(this, config);
