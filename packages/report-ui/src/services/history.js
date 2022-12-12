09faa399687b60bca2c81ca76b19f17224450cd7
Olivier <hi@olivierodo.me>
2022-12-12 22:55:18 +0700

    fix: conflicted history between microservices - report ui

--- packages/report-ui/src/services/history.js
+++ packages/report-ui/src/services/history.js
@@ -1,21 +1,24 @@
 const History = {};
 
-History.key = "RESTQA_HISTORY";
+const KEY_SUFFIX = "RESTQA_HISTORY";
+
 History.limit = 10;
 
 History.add = function (result) {
   if (!result) throw new Error("The received result is not valid");
-  const { RESTQA_RESULT } = result;
+  const { RESTQA_RESULT, RESTQA_CONFIG } = result;
+  this._microserviceCode = RESTQA_CONFIG.metadata.code;
   result.id = RESTQA_RESULT.id;
   if (this.get(result.id)) {
     return;
   }
+
   const list = this.list();
   list.push(result);
   if (list.length > this.limit) {
     list.shift(1, 0);
   }
-  localStorage.setItem(this.key, JSON.stringify(list));
+  localStorage.setItem(this.key(), JSON.stringify(list));
   return this;
 };
 
@@ -30,12 +33,19 @@ History.last = function () {
 };
 
 History.list = function () {
-  const content = localStorage.getItem(this.key);
+  const content = localStorage.getItem(this.key());
   let result = [];
   if (content) {
     result = JSON.parse(content);
   }
   return result;
+};
+
+History.key = function () {
+  if (!this._key) {
+    this._key = [this._microserviceCode, KEY_SUFFIX].join("-");
+  }
+  return this._key;
 };
 
 export default History;
